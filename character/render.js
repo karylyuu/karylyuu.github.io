5143f817d795b9803825c0000e3f852cf2ae48a7
karylyuu <167455221+karylyuu@users.noreply.github.com>
2026-04-17 23:25:06 +0900

    Update render.js

--- character/render.js
+++ character/render.js
@@ -1,11 +1,20 @@
-export function render(state, root) {
+export function render(state, root, rod) {
 
   const idleSwing = Math.sin(state.time * 1.5) * 0.01;
   const idleBounce = Math.sin(state.time * 2.2) * 2;
 
+  const totalAngle = state.angle + idleSwing;
+
+  // 캐릭터
   root.style.transform = `
     translateX(-50%)
-    rotate(${state.angle + idleSwing}rad)
+    rotate(${totalAngle}rad)
     translateY(${idleBounce}px)
+  `;
+
+  // 막대기 (살짝만 따라감)
+  rod.style.transform = `
+    translateX(-50%)
+    rotate(${totalAngle * 0.3}rad)
   `;
 }
