5b8f4f3c8d756125fbe984edc8c61bdf5d693ab7
karylyuu <167455221+karylyuu@users.noreply.github.com>
2026-04-18 13:09:09 +0900

    Update render.js

--- character/render.js
+++ character/render.js
@@ -1,14 +1,20 @@
-const pivot = document.getElementById("pivot");
-const rod = document.getElementById("rod");
+let pivot;
+let rod;
 
 export function render(state) {
 
-  // 회전
+  // 최초 1회만 가져오기
+  if (!pivot) {
+    pivot = document.getElementById("pivot");
+    rod = document.getElementById("rod");
+  }
+
+  if (!pivot || !rod) return; // 안전장치
+
   pivot.style.transform = `
     translateX(-50%)
     rotate(${state.angle}rad)
   `;
 
-  // 길이 적용
   rod.style.height = `${state.length}px`;
 }
