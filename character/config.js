1c609a4b5ce445d43c90c2d4f2345b53c93cd429
karylyuu <167455221+karylyuu@users.noreply.github.com>
2026-04-19 20:33:02 +0900

    Update config.js

--- character/config.js
+++ character/config.js
@@ -1,25 +1,25 @@
 export const config = {
-  baseLength: 52,
-  minLength: 36,
-  maxLength: 126,
+  baseLength: 50,
+  minLength: 34,
+  maxLength: 118,
 
   maxAngle: Math.PI / 4,
   dragThreshold: 5,
 
-  dragAngleFactor: 0.00235,
+  dragAngleFactor: 0.0022,
   dragLengthFactor: 0.28,
   dragEase: 0.16,
 
-  springK: 0.082,
+  springK: 0.08,
   damping: 0.905,
 
   lengthSpringK: 0.12,
   lengthDamping: 0.86,
 
-  releaseAngleFactor: 0.00022,
-  releaseLengthFactor: 0.00038,
+  releaseAngleFactor: 0.00025,
+  releaseLengthFactor: 0.00042,
 
-  idleAmplitude: 0.0035,
+  idleAmplitude: 0.003,
   idleSpeed: 6.0,
 
   charOverlap: 18
