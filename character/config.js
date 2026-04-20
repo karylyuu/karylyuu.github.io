5e9330872bc035d97971a6ac9240594ce0ff0c9f
karylyuu <167455221+karylyuu@users.noreply.github.com>
2026-04-20 18:35:52 +0900

    Update config.js

--- character/config.js
+++ character/config.js
@@ -1,27 +1,4 @@
-export const config = {
-  baseLength: 50,
-  minLength: 34,
-  maxLength: 122,
-
-  // 막대가 캐릭터 몸 안쪽으로 들어가는 깊이
-  rodHeadOverlap: 52,
-
-  maxAngle: Math.PI / 4,
-  dragThreshold: 5,
-
-  dragAngleFactor: 0.00215,
-  dragLengthFactor: 0.45,
-
-  dragAngleSpring: 0.10,
-  dragLengthSpring: 0.11,
-  dragAngleDamping: 0.90,
-  dragLengthDamping: 0.90,
-
-  releaseAngleSpring: 0.072,
-  releaseLengthSpring: 0.085,
-  releaseAngleDamping: 0.92,
-  releaseLengthDamping: 0.91,
-
+ex
   // 길이와 각도 사이에 아주 약한 곡선 감각을 주는 값
   releaseCoupling: 0.0016,
 
