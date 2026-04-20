7d569106a06aec59e47f2132b46bc61467346871
karylyuu <167455221+karylyuu@users.noreply.github.com>
2026-04-20 18:36:07 +0900

    Update state.js

--- character/state.js
+++ character/state.js
@@ -5,16 +5,4 @@ export const state = {
   angularVel: 0,
 
   length: 50,
-  lengthVel: 0,
-
-  dragging: false,
-  dragArmed: false,
-
-  mouse: { x: 0, y: 0 },
-  prevMouse: { x: 0, y: 0 },
-  dragStart: { x: 0, y: 0 },
-
-  pointerVX: 0,
-  pointerVY: 0,
-  lastMoveAt: 0
-};
+  le
